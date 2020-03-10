bc9f3b4f6056498445c4bf93f7cab2ef28494bc2
Michael J. Stallinger <at.mjst@mjst.at>
2020-03-10 23:15:54 +0100

    Alternative wait method, if Windows Resource Kit not installed, fixed typo

--- force_restart_service/forcerestartsvc.cmd
+++ force_restart_service/forcerestartsvc.cmd
@@ -1,7 +1,8 @@
 @echo off
 :: try stopping the service
 net stop %1
-sleep 5
+:: sleep 5
+CHOICE /C AB /T 5 /D A >NUL
 :: force the service to stop ;)
 taskkill /f /FI "SERVICES eq %1"
 :: (re-)start the service
